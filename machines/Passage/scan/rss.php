593e50783765dfed0b006296c1c1c29f3017b469
Rapsodia <mattiadallantonia@gmail.com>
2021-04-01 12:45:44 +0200

    box Delivey rooted

---
+++ machines/Passage/scan/rss.php
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<?xml-stylesheet type="text/css" href="http://passage.htb/CuteNews/skins/rss_style.css" ?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+<channel>
+<title>Passage RSS Feed</title>
+<link>http://passage.htb/news.php</link>
+<language>en-us</language>
+<description></description>
+<!-- <docs>This is an RSS 2.0 file intended to be viewed in a newsreader or syndicated to another site. For more information on RSS check: http://www.feedburner.com/fb/a/aboutrss</docs> -->
+<generator>CuteNews</generator>
+<atom:link href="http://passage.htb/CuteNews/rss.php" rel="self" type="application/rss+xml" /><item>
+   <title><![CDATA[**Implemented Fail2Ban**]]></title>
+   <link>http://passage.htb/news.php?id=11</link>
+   <description><![CDATA[Due to unusally large amounts of traffic,]]></description>
+   <guid isPermaLink="false">1592488043</guid>
+   <pubDate>Thu, 18 Jun 2020 09:47:23 -0400</pubDate>
+</item>
+<item>
+   <title><![CDATA[Phasellus tristique urna]]></title>
+   <link>http://passage.htb/news.php?id=8</link>
+   <description><![CDATA[Sed felis pharetra, nec sodales diam sagittis.]]></description>
+   <guid isPermaLink="false">1591987514</guid>
+   <pubDate>Fri, 12 Jun 2020 14:45:14 -0400</pubDate>
+</item>
+<item>
+   <title><![CDATA[Aenean dapibus nec]]></title>
+   <link>http://passage.htb/news.php?id=7</link>
+   <description><![CDATA[Urna eget vulputate.]]></description>
+   <guid isPermaLink="false">1591450298</guid>
+   <pubDate>Sat, 06 Jun 2020 09:31:38 -0400</pubDate>
+</item>
+<item>
+   <title><![CDATA[Nullam metus tellus]]></title>
+   <link>http://passage.htb/news.php?id=6</link>
+   <description><![CDATA[Ornare ut fringilla id, accumsan quis turpis.]]></description>
+   <guid isPermaLink="false">1588433035</guid>
+   <pubDate>Sat, 02 May 2020 11:23:55 -0400</pubDate>
+</item>
+<item>
+   <title><![CDATA[Fusce cursus, nulla in ultricies]]></title>
+   <link>http://passage.htb/news.php?id=5</link>
+   <description><![CDATA[Posuere, lectus metus ultricies neque, eu pulvinar enim nisi id tortor.]]></description>
+   <guid isPermaLink="false">1587128696</guid>
+   <pubDate>Fri, 17 Apr 2020 09:04:56 -0400</pubDate>
+</item>
+<item>
+   <title><![CDATA[Maecenas varius convallis]]></title>
+   <link>http://passage.htb/news.php?id=4</link>
+   <description><![CDATA[Nisi ut porta.]]></description>
+   <guid isPermaLink="false">1586711095</guid>
+   <pubDate>Sun, 12 Apr 2020 13:04:55 -0400</pubDate>
+</item>
+<item>
+   <title><![CDATA[Nunc facilisis ornare]]></title>
+   <link>http://passage.htb/news.php?id=3</link>
+   <description><![CDATA[Arcu quis finibus.]]></description>
+   <guid isPermaLink="false">1585405439</guid>
+   <pubDate>Sat, 28 Mar 2020 10:23:59 -0400</pubDate>
+</item>
+<item>
+   <title><![CDATA[Sed porta lectus]]></title>
+   <link>http://passage.htb/news.php?id=2</link>
+   <description><![CDATA[Vitae justo ultricies vehicula.]]></description>
+   <guid isPermaLink="false">1584459160</guid>
+   <pubDate>Tue, 17 Mar 2020 11:32:40 -0400</pubDate>
+</item>
+<item>
+   <title><![CDATA[Lorem ipsum dolor]]></title>
+   <link>http://passage.htb/news.php?id=1</link>
+   <description><![CDATA[Sit amet, consectetur adipiscing elit.]]></description>
+   <guid isPermaLink="false">1583243399</guid>
+   <pubDate>Tue, 03 Mar 2020 08:49:59 -0500</pubDate>
+</item>
+<!-- News Powered by CuteNews: http://cutephp.com/ --></channel></rss>
